8c150cb179d07324aaff2a1535f232884e92d081
NATHEE PHUWONG <nathee.phuwong@scbtechx.io>
2025-04-14 17:16:08 +0700

    update config architecture

--- messages-service/_ecosystem.config.js
+++ messages-service/_ecosystem.config.js
@@ -1,25 +1,9 @@
 module.exports = {
     apps: [{
-      name: "msg-service1",
+      name: "msg-service",
       script: "messages-service/bin/www",
       env: {
         PORT:8081,
-        NODE_ENV:"production"
-      }
-    },
-    {
-      name: "msg-service2",
-      script: "messages-service/bin/www",
-      env: {
-        PORT:8082,
-        NODE_ENV:"production"
-      }
-    },
-    {
-      name: "msg-service3",
-      script: "messages-service/bin/www",
-      env: {
-        PORT:8083,
         NODE_ENV:"production"
       }
     }]
